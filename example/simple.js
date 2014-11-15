2d9dd1d76576628dd593010e586bceedce2b3aed
Izaak Schroeder <izaak.schroeder@gmail.com>
2014-11-14 23:58:29 -0800

    Lint passing, minor updates to example to take in CLI args.

--- example/simple.js
+++ example/simple.js
@@ -1,25 +1,30 @@
 
-var path = require('path'),
+'use strict';
+
+var argv = require('yargs'),
+	path = require('path'),
 	through2 = require('through2'),
 	sqs = require(path.join(__dirname, '..'));
 
 function work(job, encoding, callback) {
 	console.log('workin on', job);
 	var self = this;
-	setTimeout(function() {
+	// Finish the job after 1 second.
+	setTimeout(function done() {
 		self.push(job);
 		callback();
 	}, 1000);
 }
 
 
-var jobs = sqs('https://sqs.us-west-2.amazonaws.com/534692210857/test');
+// Specify SQS queue url as --queue=https://...
+var jobs = sqs(argv.queue);
 var worker = through2({ objectMode: true, highWaterMark: 1 }, work);
 jobs.pipe(worker);
 
 
 var count = 0;
-
+// Inject jobs into the queue every 3 seconds.
 setInterval(function job() {
-	jobs.write({ type: 'test', data: { message: 'hello '+count } });
+	jobs.write({ type: 'test', data: { message: 'hello ' + count } });
 }, 3000);
